0117355fe0168e42eae9268fbd7cb68aa8cf3864
harshavarma29 <harshavarma29@gmail.com>
2026-09-07 10:59:20 +0530

    Added file replace to build.gradle.kts file

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,3 +41,7 @@ kotlin {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+	archiveFileName.set("app.jar")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,8 +2,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
-tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
-    archiveFileName.set("app.jar")
-}
-
 rootProject.name = "firstApp"
