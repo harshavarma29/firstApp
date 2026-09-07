3900179ab73341811fbe0153cc9f1e01f92df846
harshavarma29 <harshavarma29@gmail.com>
2026-09-07 10:50:35 +0530

    Changed the file JenkinsFile and Dockerfile to extract on Jar file

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM eclipse-temurin:17-jre-alpine
 VOLUME /tmp
-COPY build/libs/*.jar app.jar
+COPY build/libs/app.jar app.jar
 ENTRYPOINT ["java", "-jar", "app.jar"]
 EXPOSE 8080
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,8 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    archiveFileName.set("app.jar")
+}
+
 rootProject.name = "firstApp"
